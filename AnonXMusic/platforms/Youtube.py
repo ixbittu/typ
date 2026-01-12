a489ddfa34093a0cb20baf4ab9e5bc3d6d8260d5
ixrocky <shivamlkr629@gmail.com>
2026-01-12 19:35:19 +0530

    Update Youtube.py

--- AnonXMusic/platforms/Youtube.py
+++ AnonXMusic/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from SaregamaMusic.utils.formatters import time_to_seconds
+from AnonXMusic.utils.formatters import time_to_seconds
 import aiohttp
 from AnonXMusic import LOGGER
 
